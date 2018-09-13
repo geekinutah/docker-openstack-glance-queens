c9d782e45a5f4b7e62c1a6a0dab07cb59706bac8
Mike Wilson <geekinutah@gmail.com>
2018-09-13 15:59:14 -0600

    Add services and templates to entrypoint

--- start_glance.sh
+++ start_glance.sh
@@ -3,3 +3,10 @@
 export HOST_ADDRESS=`hostname -I | sed 's/^[[:space:]]*//g'`
 cat /openrc.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /openrc
 
+for i in cache glare manage registry; do
+    cat /etc/glance/glance-$i.conf.j2 | python -c 'import os;import sys; import jinja2; sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))' > /etc/glance/glance-$i.conf;
+done
+
+/usr/local/bin/glance-glare --config-file=/etc/glance/glance-glare.conf &
+/usr/local/bin/glance-registry --config-file=/etc/glance/glance-registry.conf &
+/usr/local/bin/glance-api --config-file=/etc/glance/glance-api.conf
